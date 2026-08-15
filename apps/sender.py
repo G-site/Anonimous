124bb057eebc2738827582ff8d4007ae31fdb274
G-site <httydverse@gmail.com>
2026-08-15 23:06:40 +0300

    Update sender.py

--- apps/sender.py
+++ apps/sender.py
@@ -1,9 +1,11 @@
 import asyncio
-from aiogram.types import FSInputFile
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from apscheduler.triggers.cron import CronTrigger
 from zoneinfo import ZoneInfo
 from bot_instance import bot
+
+
+from apps.database import regenerate_tech_promo
 
 
 CHAT_ID = -1003219172904
@@ -11,6 +13,7 @@ PHOTO_PATH = "https://i.postimg.cc/3RtF5xQy/photo-2026-03-13-16-50-35.jpg"
 
 
 async def weekly_message():
+    await regenerate_tech_promo()
     await bot.send_photo(
         chat_id=CHAT_ID,
         photo=PHOTO_PATH,
